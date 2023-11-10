4f4e824ef7e14a7f9d63c8ff556f856a6513898a
José Abraão dos Santos Oliveira <122422422+Abraao2005@users.noreply.github.com>
2023-11-10 11:49:00 -0300

    Add files via upload

--- css/css.php
+++ css/css.php
@@ -1,1 +1,53 @@
+<style>
 
+*{
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+
+}
+#main{
+    height: 100vh;
+    width: 100vw;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: gray;
+}
+form{
+    width: 300px;
+    height: auto;
+    padding: 15px;
+    background-color: white;
+}
+h2{
+font-family: 'Courier New', Courier, monospace;
+text-align: center;
+padding: 15px;
+}
+.label{
+    display: flex;
+    flex-direction: column;
+    text-align: center;
+    align-items: center;
+    font: 18px monospace;
+    margin: 15px;
+}
+.label input{
+    width: 200px;
+    padding: 3px;
+}
+#btn-submit{
+    padding: 5px;
+    width: 60px;
+    background-color: gray;
+    border: 1px solid white;
+    color: white;
+
+}
+#btn-submit:hover{
+    background-color: #a9a9a9;
+    border: 1px solid white;
+    color: black;
+}
+</style>
